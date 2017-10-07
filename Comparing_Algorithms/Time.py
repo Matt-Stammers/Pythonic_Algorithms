4747df958b9458ce28e1552ef2cf8184de8918f8
Matt Stammers <matthewstammers@googlemail.com>
2017-10-07 21:14:54 +0100

    Create Time.py

---
+++ Comparing_Algorithms/Time.py
@@ -0,0 +1,14 @@
+# One way to compare algorithms is by timing them. Their example is below:
+
+import time
+
+def sumOfN2(n):
+   start = time.time()
+
+   theSum = 0
+   for i in range(1,n+1):
+      theSum = theSum + i
+
+   end = time.time()
+
+   return theSum,end-start
