8918722c0595ea4437e9125715ead195f3ab7533
Matt Stammers <matthewstammers@googlemail.com>
2017-10-12 20:59:23 +0100

    Create Stack_implementation.py

---
+++ Recursion/Stack_implementation.py
@@ -0,0 +1,18 @@
+# First you need to import or build the stack class: Then:
+
+rStack = Stack()
+
+def toStr(n,base):
+    convertString = "0123456789ABCDEF"
+    while n > 0:
+        if n < base:
+            rStack.push(convertString[n])
+        else:
+            rStack.push(convertString[n % base])
+        n = n // base
+    res = ""
+    while not rStack.isEmpty():
+        res = res + str(rStack.pop())
+    return res
+
+print(toStr(1453,16))
