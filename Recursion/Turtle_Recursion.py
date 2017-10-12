953b945d5eefe7092be3dfc37165152e5bf8def1
Matt Stammers <matthewstammers@googlemail.com>
2017-10-12 22:23:41 +0100

    Create Turtle_Recursion.py

---
+++ Recursion/Turtle_Recursion.py
@@ -0,0 +1,46 @@
+# This visually demonstrates recursion using turtle:
+
+import turtle
+
+myTurtle = turtle.Turtle()
+myWin = turtle.Screen()
+
+def drawSpiral(myTurtle, lineLen):
+    if lineLen > 0:
+        myTurtle.forward(lineLen)
+        myTurtle.right(90)
+        drawSpiral(myTurtle,lineLen-5)
+
+drawSpiral(myTurtle,200)
+myWin.exitonclick()
+
+# as you can see the turtle spins in itself until it reaches a centrepoint.
+
+# now we can use recursion to build a 'fractal' (something that looks the same at all degrees of magnification like a coastline). 
+# we can make a tree using turtle:
+
+import turtle
+
+def tree(branchLen,t):
+    if branchLen > 5:
+        t.forward(branchLen)
+        t.right(20)
+        tree(branchLen-15,t)
+        t.left(40)
+        tree(branchLen-15,t)
+        t.right(20)
+        t.backward(branchLen)
+
+def main():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    t.color("green")
+    tree(75,t)
+    myWin.exitonclick()
+
+main()
+
