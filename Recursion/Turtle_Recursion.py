a20b2a174d8aa5df33570da0c4d760757b6a5cc3
Matt Stammers <matthewstammers@googlemail.com>
2017-10-12 22:34:05 +0100

    Update Turtle_Recursion.py

--- Recursion/Turtle_Recursion.py
+++ Recursion/Turtle_Recursion.py
@@ -16,7 +16,7 @@ myWin.exitonclick()
 
 # as you can see the turtle spins in itself until it reaches a centrepoint.
 
-# now we can use recursion to build a 'fractal' (something that looks the same at all degrees of magnification like a coastline). 
+# now we can use recursion to build a 'fractal' (something that looks the same at all degrees of magnification like a coastline - property of self-similarity.). 
 # we can make a tree using turtle:
 
 import turtle
@@ -45,3 +45,30 @@ def main():
 main()
 
 # this creates a wonky tree. You can experiment with all sorts of other options
+
+# This will make a much thinner tree:
+
+import turtle
+
+def tree(branchLen,t):
+    if branchLen > 5:
+        t.forward(branchLen)
+        t.right(15)
+        tree(branchLen-15,t)
+        t.left(30)
+        tree(branchLen-15,t)
+        t.right(15)
+        t.backward(branchLen)
+
+def main():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(150)
+    t.down()
+    t.color("green")
+    tree(85,t)
+    myWin.exitonclick()
+
+main()
